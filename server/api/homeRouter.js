c8806a82a6083d8c7eabe83c14d01c30fe01bcdd
marlonaesparza <marlonaesparza@gmail.com>
2021-09-14 14:35:53 -0700

    Uncommented home router logic.

--- server/api/homeRouter.js
+++ server/api/homeRouter.js
@@ -3,26 +3,25 @@ const axios = require('axios');
 const homeRouter = express.Router();
 
 homeRouter.get('/', (req, res) => {
-  // return axios.get('http://localhost:8001/auth', {
-  //   params: {
-  //     session: req.cookies.hpp_session
-  //   }
-  // })
-  // .then(({ data }) => {
-  //   let { session } = data;
+  return axios.get('http://localhost:8001/auth', {
+    params: {
+      session: req.cookies.hpp_session
+    }
+  })
+  .then(({ data }) => {
+    let { session } = data;
     
-  //   if (!session.userUUID) {
-  //     throw session;
-  //   };
+    if (!session.userUUID) {
+      throw session;
+    };
     
-  //   // TODO: get variables from Users Service
-  //   return res.render('pages/home');
-  // })
-  // .catch((session) => {
-  //   res.cookie('hpp_session', session);
-  //   return res.redirect('/login');
-  // });
-  res.render('pages/home');
+    // TODO: get variables from Users Service
+    return res.render('pages/home');
+  })
+  .catch((session) => {
+    res.cookie('hpp_session', session);
+    return res.redirect('/login');
+  });
 });
 
 module.exports = homeRouter;
